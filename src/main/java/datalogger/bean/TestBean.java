8678e60168bbad405770df7ff7868b18898ea994
Lars Nordberg <lars@iMac5k.local>
2015-01-04 23:32:43 +0100

    commence Faces

---
+++ src/main/java/datalogger/bean/TestBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datalogger.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author lars
+ */
+@ManagedBean(name = "TestBean")
+@SessionScoped
+public class TestBean {
+    private String data = "123";
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    
+}
