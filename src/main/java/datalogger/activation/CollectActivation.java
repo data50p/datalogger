65fff73146833b85680035c0ebd3d879e4bb4ec5
Lars Nordberg <lars@femtioprocent.se>
2016-01-24 14:48:38 +0100

    process standard messages

--- src/main/java/datalogger/activation/CollectActivation.java
+++ src/main/java/datalogger/activation/CollectActivation.java
@@ -29,113 +29,108 @@ public class CollectActivation extends Activation {
 
     class CollectorClient extends PropagandaClient {
 
-	CollectorClient() {
-	    super("CollectorClient");
-	}
+        CollectorClient() {
+            super("CollectorClient");
+        }
 
-	void start() {
-	    try {
-		String hostname = "unknown";
-		try {
-		    hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException ex) {
-		}
-		sendMsg(new Datagram(anonymousAddrType,
-			serverAddrType,
-			register,
-			new Message("dl-collector-" + hostname
-				+ "@DATALOGGER")));
-		for (;;) {
-		    Datagram datagram = getConnector().recvMsg();
-		    S.pL("Collector got: " + datagram);
-		    if (datagram == null) {
-			break;
-		    }
-		    if (datagram.getMessageType() == MessageType.ping) {
-			sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.pong, datagram.getMessage()));
-			System.err.println("got datagram: " + name + " =----> PING " + datagram);
-		    } else if (datagram.getMessageType() == MessageType.pong) {
-			System.err.println("got datagram: " + name + " =----> PONG " + datagram);
-		    } else if (datagram.getMessageType() == MessageType.plain) {
-			System.err.println("got datagram: " + name + " =----> " + datagram);
-			String msg = datagram.getMessage().getMessage();
-			String msgArr[] = datagram.getMessage().getAddendum().split(" ");
-			if ("log".equals(msg)) {
-			    // log add <dev> <type> <value>
-			    if (msgArr.length == 4 && msgArr[0].equals("add")) {
-				try {
-				    Double val = Double.parseDouble(msgArr[3].replace(",", "."));
-				    int id = log(msgArr[1], msgArr[2], val);
-				    Message rmsg;
-				    if (id == 0) {
-					rmsg = new Message("logged", "NOT added");
-				    } else if (id == -1) {
-					rmsg = new Message("logged", "SAME not added");
-				    } else if (id < 0) {
-					rmsg = new Message("logged", "NOT added, error: " + id);
-				    } else {
-					rmsg = new Message("logged", "added id " + id);
-				    }
-				    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
-				} catch (Exception ex) {
-				    Message rmsg = new Message("logged", "NOT added " + ex);
-				    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
-				}
+        void start() {
+            try {
+                String hostname = "unknown";
+                try {
+                    hostname = InetAddress.getLocalHost().getHostName();
+                } catch (UnknownHostException ex) {
+                }
+                sendMsg(new Datagram(anonymousAddrType,
+                        serverAddrType,
+                        register,
+                        new Message("dl-collector-" + hostname
+                                + "@DATALOGGER")));
+                for (;;) {
+                    Datagram datagram = getConnector().recvMsg();
+                    S.pL("Collector got: " + datagram);
+                    if (datagram == null) {
+                        break;
+                    }
+                    if (standardProcessMessage(datagram, MessageType.plain) == MessageTypeFilter.FILTERED) {
+                        System.err.println("got datagram: " + name + " =----> " + datagram);
+                        String msg = datagram.getMessage().getMessage();
+                        String msgArr[] = datagram.getMessage().getAddendum().split(" ");
+                        if ("log".equals(msg)) {
+                            // log add <dev> <type> <value>
+                            if (msgArr.length == 4 && msgArr[0].equals("add")) {
+                                try {
+                                    Double val = Double.parseDouble(msgArr[3].replace(",", "."));
+                                    int id = log(msgArr[1], msgArr[2], val);
+                                    Message rmsg;
+                                    if (id == 0) {
+                                        rmsg = new Message("logged", "NOT added");
+                                    } else if (id == -1) {
+                                        rmsg = new Message("logged", "NOT added, same");
+                                    } else if (id < 0) {
+                                        rmsg = new Message("logged", "NOT added, error: " + id);
+                                    } else {
+                                        rmsg = new Message("logged", "added id " + id);
+                                    }
+                                    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
+                                } catch (Exception ex) {
+                                    Message rmsg = new Message("logged", "NOT added: " + ex);
+                                    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
+                                }
                             } else if (msgArr.length == 5 && msgArr[0].equals("addT")) {
                                 // log addT <timestamp> <dev> <type> <value>
-				try {
+                                try {
                                     Long tm = Long.parseLong(msgArr[1]);
                                     Date timestamp = new Date(tm);
-				    Double val = Double.parseDouble(msgArr[4].replace(",", "."));
-				    int id = log(msgArr[2], msgArr[3], val, timestamp);
-				    Message rmsg;
-				    if (id == 0) {
-					rmsg = new Message("logged", "NOT added");
-				    } else if (id == -1) {
-					rmsg = new Message("logged", "SAME not added");
-				    } else if (id < 0) {
-					rmsg = new Message("logged", "NOT added, error: " + id);
-				    } else {
-					rmsg = new Message("logged", "added id " + id);
-				    }
-				    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
-				} catch (Exception ex) {
-				    Message rmsg = new Message("logged", "NOT added " + ex);
-				    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
-				}
-			    } else {
-				Message rmsg = new Message("error", "format");
-				sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
-			    }
-			}
-		    } else {
-			System.err.println("got datagram: _ " + name + " =----> " + datagram);
-		    }
-		}
-	    } catch (PropagandaException ex) {
-		S.pL("CollectorClient: " + ex);
-	    }
-	}
+                                    Double val = Double.parseDouble(msgArr[4].replace(",", "."));
+                                    int id = log(msgArr[2], msgArr[3], val, timestamp);
+                                    Message rmsg;
+                                    if (id == 0) {
+                                        rmsg = new Message("logged", "NOT added");
+                                    } else if (id == -1) {
+                                        rmsg = new Message("logged", "NOT added, same");
+                                    } else if (id < 0) {
+                                        rmsg = new Message("logged", "NOT added, error: " + id);
+                                    } else {
+                                        rmsg = new Message("logged", "added id " + id);
+                                    }
+                                    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
+                                } catch (Exception ex) {
+                                    Message rmsg = new Message("logged", "NOT added: " + ex);
+                                    sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
+                                }
+                            } else {
+                                Message rmsg = new Message("error", "format");
+                                sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.plain, rmsg));
+                            }
+                        }
+                    } else {
+                        System.err.println("got datagram: _ " + name + " =----> " + datagram);
+                    }
+                }
+            } catch (PropagandaException ex) {
+                S.pL("CollectorClient: " + ex);
+            }
+        }
     }
 
     public void doCollecting() {
-	final Connector_Plain conn = (Connector_Plain) PropagandaConnectorFactory.create("Plain", "Collector", null, null);
-	final CollectorClient client = new CollectorClient();
-	System.err.println("Connect propaganda: " + Appl.flags.get("p.host") + ' ' + Integer.parseInt(Appl.flags.get("p.port")));
-	if (conn.connect(Appl.flags.get("p.host"), Integer.parseInt(Appl.flags.get("p.port")))) {
-	    client.setConnector(conn);
-	    conn.attachClient(client);
-	    S.pL("conn " + conn);
-	    Thread th2 = new Thread(() -> client.start());
-	    th2.start();
-	} else {
-	    System.err.println("No connection to propaganda: " + Appl.flags.get("p.host") + ' ' + Integer.parseInt(Appl.flags.get("p.port")));
-	    System.exit(1);
-	}
+        final Connector_Plain conn = (Connector_Plain) PropagandaConnectorFactory.create("Plain", "Collector", null, null);
+        final CollectorClient client = new CollectorClient();
+        System.err.println("Connect propaganda: " + Appl.flags.get("p.host") + ' ' + Integer.parseInt(Appl.flags.get("p.port")));
+        if (conn.connect(Appl.flags.get("p.host"), Integer.parseInt(Appl.flags.get("p.port")))) {
+            client.setConnector(conn);
+            conn.attachClient(client);
+            S.pL("conn " + conn);
+            Thread th2 = new Thread(() -> client.start());
+            th2.start();
+        } else {
+            System.err.println("No connection to propaganda: " + Appl.flags.get("p.host") + ' ' + Integer.parseInt(Appl.flags.get("p.port")));
+            System.exit(1);
+        }
     }
 
     @Override
     public void start() {
-	doCollecting();
+        doCollecting();
     }
 }
--- src/main/java/datalogger/activation/CrontabActivation.java
+++ src/main/java/datalogger/activation/CrontabActivation.java
@@ -72,18 +72,13 @@ public class CrontabActivation extends Activation {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
-                for (; ; ) {
+                for (;;) {
                     Datagram datagram = getConnector().recvMsg();
                     S.pL("Crontab got: " + datagram);
                     if (datagram == null) {
                         break;
                     }
-                    if (datagram.getMessageType() == MessageType.ping) {
-                        sendMsg(new Datagram(getDefaultAddrType(), datagram.getSender(), MessageType.pong, datagram.getMessage()));
-                        System.err.println("got datagram: " + name + " =----> PING " + datagram);
-                    } else if (datagram.getMessageType() == MessageType.pong) {
-                        System.err.println("got datagram: " + name + " =----> PONG " + datagram);
-                    } else if (datagram.getMessageType() == MessageType.plain) {
+                    if (standardProcessMessage(datagram, MessageType.plain) == MessageTypeFilter.FILTERED) {
                         System.err.println("got datagram: " + name + " =----> " + datagram);
                         String msg = datagram.getMessage().getMessage();
                         String msgArr[] = datagram.getMessage().getAddendum().split(" ");
